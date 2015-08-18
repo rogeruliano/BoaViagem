81aefe007784111f534c1995f91ed6154b786a62
Roger <roger.b.uliano@gmail.com>
2015-08-18 10:50:06 -0300

    Commit Inicial Projeto Boa Viagem

---
+++ BoaViagem/app/src/main/java/com/rogeruliano/boaviagem/BoaViajemActivity.java
@@ -0,0 +1,39 @@
+package com.rogeruliano.boaviagem;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Created by uliano.roger on 22/07/2015.
+ */
+public class BoaViajemActivity extends Activity {
+
+    private EditText usuario;
+    private EditText senha;
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        usuario = (EditText) findViewById(R.id.usuario);
+        senha = (EditText) findViewById(R.id.senha);
+    }
+
+    public void entrarOnClick(View v) {
+
+        String usuarioInformado = usuario.getText().toString();
+        String senhaInformada = senha.getText().toString();
+
+        if (usuarioInformado == "leitor" && senhaInformada == "123") {
+
+        } else {
+            String menssageErro = getString(R.string.erro_autenticacao);
+            Toast toast = Toast.makeText(this, menssageErro, Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+    }
+}
